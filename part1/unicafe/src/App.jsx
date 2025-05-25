290b386528291ffb7b8b098787c86c6b4d0e1426
Miguel Colque <micky.cm@outlook.es>
2025-05-25 18:26:29 -0400

    Ejercicio 1.7 - Parte 1: Ampliación para mostrar promedio y porcentaje de positivos

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,17 +9,24 @@ const App = ()=> {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive = total === 0 ? 0 : (good * 100) / total
+
   return (
     <>
       <h1>Give feedback</h1>
       <Button onClick={()=> setGood(good + 1)} text="good" />
-      <Button onClick={()=> setNeutral(neutral + 1)} text="netral" />
+      <Button onClick={()=> setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={()=> setBad(bad + 1)} text="bad" />
 
       <h2>Statistics</h2>
       <StatisticParagraph text="Good: " value={good} />
       <StatisticParagraph text="Neutral: " value={neutral} />
       <StatisticParagraph text="Bad: " value={bad} />
+
+      <StatisticParagraph text="Average: " value={average} />
+      <StatisticParagraph text="Positive: " value={positive + ' %'} />
     </>
   )
 }
