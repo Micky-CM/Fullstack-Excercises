bb1c54c856323f0a2355f0fc6a306377161d4d8a
Miguel Colque <micky.cm@outlook.es>
2025-05-30 20:21:27 -0400

    Ejercicio 2.5. - Parte 2: Módulo separado para el componente Course

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,31 +1,4 @@
-const Header = ({course})=> <h1>{course.name}</h1>
-
-const Part = ({part})=> <li>{part.name}: {part.exercises}</li>
-
-const Content = ({course})=> {
-  return (
-    <ul>
-      {course.parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-    </ul>
-  )
-}
-
-const Total = ({course})=> {
-  const total = course.parts.reduce((sum, part)=> sum + part.exercises, 0)
-  return <p><strong>Total of {total} exercises</strong></p>
-}
-
-const Course = ({course})=> {
-  return (
-    <section>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </section>
-  )
-}
+import Course from "./components/course"
 
 const App = ()=> {
   const courses = [
