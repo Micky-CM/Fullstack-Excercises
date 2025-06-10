0d933c57306697972a1df26a7cd8d211dcdc4020
Miguel Colque <micky.cm@outlook.es>
2025-06-09 21:35:06 -0400

    Ejercicio 2.6 - Parte 2: Aplicación para agregar nombres a una agenda telefónica

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const Person = ({name})=> <li>{name}</li>
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event)=> {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons([...persons, newPerson])
+    setNewName('')
+  }
+
+  const handleNameChange = (event)=> {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          Name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {
+          persons.map(person => (
+            <Person key={person.name} name={person.name} />
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App
