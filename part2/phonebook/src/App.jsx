084049d0787197b1098e46b6990f99aa334c7b00
Miguel Colque <micky.cm@outlook.es>
2025-06-15 20:25:57 -0400

    Ejercicio 2.11 - Parte 2: Simular un servidor y obtener el estado inicial mediante axios con useEffect

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
-const App = ({contacts}) => {
-  const [persons, setPersons] = useState(contacts)
+const App = () => {
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+
+  useEffect(()=> {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response=> {
+      setPersons(response.data)
+    })
+  }, [])
 
   const addPerson = (event)=> {
     event.preventDefault()
